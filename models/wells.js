6acdad6394e0b0813d9e0d2a370bf3e92dc563c5
momadison <mattomadison.gmail.com>
2019-05-27 20:30:37 -0500

    get models

--- models/wells.js
+++ models/wells.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
           type: DataTypes.STRING,
           allowNull: false
       }, 
-      apiNumber: {
+      apiNumber: { 
           type: DataTypes.STRING,
           allowNull: false
       },
